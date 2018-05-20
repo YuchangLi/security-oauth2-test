e1351752fa34a6e5e80b939fb34bca8a30af7dd3
li_yc <648821884@qq.com>
2018-05-21 00:05:06 +0800

    add zull

---
+++ security-test-sso-zuul/src/main/java/com/liyc/security/sso/zuul/SecurityTestSsoZuulApplication.java
@@ -0,0 +1,19 @@
+package com.liyc.security.sso.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+//@EnableOAuth2Sso
+@EnableZuulProxy
+public class SecurityTestSsoZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTestSsoZuulApplication.class, args);
+	}
+	
+}
--- security-test-sso/src/main/java/com/liyc/security/sso/SsoClient1Application.java
+++ security-test-sso/src/main/java/com/liyc/security/sso/SsoClient1Application.java
@@ -6,11 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.ui.Model;
@@ -30,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SsoClient1Application {
 
     @Autowired
-    OAuth2RestTemplate oAuth2RestTemplate;
+    OAuth2RestOperations oAuth2RestTemplate;
 
 
 //    @Value("${messages.url:http://sso-resource:8085}/resource")
@@ -66,7 +64,7 @@ public class SsoClient1Application {
     OAuth2RestTemplate oAuth2RestTemplate(OAuth2ClientContext oAuth2ClientContext, OAuth2ProtectedResourceDetails details){
         return new OAuth2RestTemplate(details,oAuth2ClientContext);
     }
-    
+//    
 //    @Configuration
 //    public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //      @Override
