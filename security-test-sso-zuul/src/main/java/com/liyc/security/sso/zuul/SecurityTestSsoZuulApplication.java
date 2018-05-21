502d5dc90c1279481984201edbf748ab8c50dfdb
liyuchang <648821884@qq.com>
2018-05-21 18:42:31 +0800

    add key-value

--- security-test-sso-zuul/src/main/java/com/liyc/security/sso/zuul/SecurityTestSsoZuulApplication.java
+++ security-test-sso-zuul/src/main/java/com/liyc/security/sso/zuul/SecurityTestSsoZuulApplication.java
@@ -2,7 +2,6 @@ package com.liyc.security.sso.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.stereotype.Controller;
 
